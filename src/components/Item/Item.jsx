02e95c1e35703345e2473f7d2299f3fa9667610d
Alan Mazunik <admazunik@gmail.com>
2022-09-26 13:56:07 -0300

    agregado firebase

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -8,9 +8,9 @@ const Item = ({ product }) => {
     return (
         <div className="card d-flex flex-column align-items-center">
             <img src={product.pictureURL} className="card-img-top" alt={product.title} />
-            <div className="card-body">
+            <div className="card-body text-center">
                 <h5 className="card-title">{product.title}</h5>
-                <p className="card-text">${product.price}.-</p>
+                <p className="card-text text-center">${product.price}.-</p>
                 <Link to={"item/" + product.id}><button className="btn btn-primary">Ver Detalles</button></Link>
             </div>
         </div>
